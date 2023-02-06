59a688c633cc5cb93c9234969a952beff2b397bc
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-06 18:49:05 +0000

    Fixed Dependencies

--- src/main/java/com/rabbitminers/extendedtrains/ExtendedTrains.java
+++ src/main/java/com/rabbitminers/extendedtrains/ExtendedTrains.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExtendedTrains.MODID)
@@ -43,6 +44,7 @@ public class ExtendedTrains {
         ExtendedTrainsBlocks.register();
         ExtendedTrainsItems.register(eventBus);
         ExtendedTrainsTileEntities.register();
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {}
